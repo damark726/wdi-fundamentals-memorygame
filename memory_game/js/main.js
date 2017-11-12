140c7f4d63d205afdb248e29f2492f6ac2bbf529
David Markoc <davidmarkoc@Davids-MBP.fios-router.home>
2017-11-11 22:55:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
 //console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+// Check to make sure all is good so far
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+// Check to make sure all is good so far
+console.log("User flipped " + cardTwo);
+
+console.log(cardsInPlay.length);
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again.");
+	}
+} else {
+}
